3741f6bd6b41890875dcf1da4359fd20ac42f905
Htet Htet Hlaing <htethtethlaing9927@gmail.com>
2023-12-26 23:53:52 +0630

    Link and images change

--- src/Project.js
+++ src/Project.js
@@ -2,25 +2,25 @@ import React from 'react';
 
 const projectsData = [
     {
-        title:'Guess Number Game',
-        image:'./Capture1.JPG',
-        link:'https://gorgeous-figolla-27710f.netlify.app/',
+        title:'To-Do-List',
+        image:'./Capture5.JPG',
+        link:'https://htethtethlaing99.github.io/Simpletodolist/',
         description:
-        'The Guess Number Game employs HTML for structure and CSS for styling, while its core functionality and interactivity are powered by JavaScript. '
+        'The To-Do-List employs HTML for structure and CSS for styling, while its core functionality and interactivity are powered by JavaScript. '
     },
 
     {
-        title:'Add To Cart',
-        image:'./Capture2.JPG',
-        link:'https://gorgeous-figolla-27710f.netlify.app/',
+        title:'Change Background Color',
+        image:'./Capture6.JPG',
+        link:'https://htethtethlaing99.github.io/change-bg-color/',
         description:
-        'Add to Cart Simple app is built with HTML for structure, CSS for styling, and JavaScript for both functionality and interactivity.'
+        'The Change background color is built with HTML for structure, CSS for styling, and JavaScript for both functionality and interactivity.'
     },
 
     {
         title:'Plannco Home Decoration',
         image:'./Capture3.JPG',
-        link:'https://gorgeous-figolla-27710f.netlify.app/',
+        link:'https://spiffy-cascaron-1bb038.netlify.app/',
         description:
         'Plannco Hoem Decoration project- HTML for structure, CSS for style, Bootstrap for responsiveness, and JavaScript for dynamic interactions, user-friendly website for home decoration.'
     },
@@ -28,7 +28,7 @@ const projectsData = [
     {
         title:'Vegonia University Website',
         image:'./Capture4.JPG',
-        link:'https://gorgeous-figolla-27710f.netlify.app/',
+        link:'https://inquisitive-belekoy-5a481f.netlify.app/',
         description:
         'Vegonia University website seamlessly integrates HTML for structure, CSS for style, Bootstrap for responsiveness, and JavaScript for interactive elements.'
     }
